fad7407e858b31a8fd494b91899a71b353105a47
TrentonH <trentonwhamilton@gmail.com>
2018-03-17 10:11:00 -0600

    added loop

--- main.py
+++ main.py
@@ -2,10 +2,20 @@ __author__ = 'Trenton'
 
 import network2
 import mnist_loader
+import matplotlib.pyplot as plt
+
+
 
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 
+#pass in a list for how many nodes in each layer
 net = network2.Network([784, 30, 10])
 
-evaluation_cost, evaluation_accuracy, training_cost, training_accuracy= net.SGD(training_data, 5, 10, 3.0, evaluation_data=test_data, monitor_training_accuracy=True,
-               monitor_evaluation_accuracy= True, monitor_evaluation_cost=True,monitor_training_cost=True)
+evaluationAccList = []
+for x in range (10, 60, 10):
+    #first number is the number of epochs the second number is mini bach- or the bach size, the last number is the learning rate
+    evaluation_cost, evaluation_accuracy, training_cost, training_accuracy= net.SGD(training_data, 2, 10, 1.0, evaluation_data=test_data, monitor_training_accuracy=False,
+                 monitor_evaluation_accuracy= True, monitor_evaluation_cost=False,monitor_training_cost=False)
+    evaluationAccList.append(evaluation_accuracy)
+
+plt.hist(evaluationAccList)
